263555dc185073e635428859bbf4cd6d908f706a
f117karikari <f117karikari@gmail.com>
2014-08-28 10:39:19 +0900

    てｓｔ

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -14,6 +14,7 @@ public class HelloWorld {
 		System.out.println(Consts.helloGoodBye);
 		System.out.println(Consts.testGit);
 		System.out.println(new Date());	
+		System.out.println("testTest");
 		System.out.println("test");
 		System.exit(Consts.EXIT_OK);
 
