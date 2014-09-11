cdf2c28cbae4ab29eccde5db509f37008cce1e08
synchro103 <igarashi@synchro-food.co.jp>
2014-09-11 10:15:57 +0900

    改行テスト
    ああ
    
    いいい

--- src/test/CalcTest.java
+++ src/test/CalcTest.java
@@ -10,21 +10,21 @@ public class CalcTest {
 	@Test
 	public void 掛け算テスト() {
 		int kekka = 12;
-		assertSame(kekka,new Calc().kake(3, 4));
+		assertSame(kekka,new Calc().kakezan(3, 4));
 	}
 	@Test
 	public void 掛け算テスト2() {
 		int kekka = 30;
-		assertSame(kekka,new Calc().kake(15, 2));
+		assertSame(kekka,new Calc().kakezan(15, 2));
 	}
 	@Test
 	public void 割り算テスト() {
 		int kekka = 5;
-		assertSame(kekka,new Calc().wari(15, 3));
+		assertSame(kekka,new Calc().warizan(15, 3));
 	}
 	@Test
 	public void ゼロ除算テスト() {
 		int kekka = 0;
-		assertSame(kekka,new Calc().wari(15, 0));
+		assertSame(kekka,new Calc().warizan(15, 0));
 	}
 }
